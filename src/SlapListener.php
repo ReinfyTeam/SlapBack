873fef4fdc6dca3576c22b7eb5283f324088db05
junjun2527 <xqwtxon@hotmail.com>
2022-04-15 11:14:25 +0800

    Fix Class

--- src/SlapListener.php
+++ src/SlapListener.php
@@ -7,17 +7,28 @@ use slapper\entities\SlapperHuman;
 use slapper\events\SlapperHitEvent;
 use xqwtxon\SlapBack\SlapListener;
 use pocketmine\event\Listener;
+use pocketmine\entity\Human;
 use pocketmine\network\mcpe\protocol\AnimatePacket;
 
 class SlapListener implements Listener {
+    
+    public function __construct(private Main $plugin){
+        //NOOP
+    }
+    
     public function onSlapperHit(SlapperHitEvent $ev){
-		$entity = $ev->getEntity();
-		if(!$entity instanceof SlapperHuman){
+        $toggle = $this->plugin->getConfig()->get("enabled");
+        if ($toggle == false) return;
+        if ($toggle == true){
+		  $entity = $ev->getEntity();
+		  if(!$entity instanceof SlapperHuman){
 			return;
-		}
-		$pk = new AnimatePacket();
-		$pk->entityRuntimeId = $entity->getId();
-		$pk->action = AnimatePacket::ACTION_SWING_ARM;
-		$ev->getDamager()->dataPacket($pk);
+		  }
+		  $pk = new AnimatePacket();
+		  $pk->entityRuntimeId = $entity->getId();
+	      $pk->action = AnimatePacket::ACTION_SWING_ARM;
+		  $ev->getDamager()->dataPacket($pk);
+		  return;
+          }
 	}
 }
