f3c04f8dbfb5b68c87a191eca92015bdfb1ecf9d
Ace Sobremont <100104970+xqwtxon@users.noreply.github.com>
2022-05-13 13:59:15 +0800

    Lmao

--- src/Main.php
+++ src/Main.php
@@ -3,46 +3,34 @@
 namespace xqwtxon\SlapBackV2;
 
 use pocketmine\plugin\PluginBase;
-use xqwtxon\SlapBackV2\SlapInfo;
 use xqwtxon\SlapBackV2\SlapListener;
 use pocketmine\utils\TextFormat;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\VersionInfo;
 
-class Main extends PluginBase implements SlapInfo{
+class Main extends PluginBase {
     public function onLoad() :void {
         $this->saveResource("config.yml");
         $log = $this->getLogger();
         $config = $this->getConfig();
-        if ($config->get("config-version") == SlapInfo::CONFIG_VERSION){
+        if ($config->get("config-version") == "1.0.0"){
             @rename($this->getDataFolder()."/"."config.yml", $this->getDataFolder()."/"."old-config.yml");
             $log->notice("Your configuration is outdated! The configuration was renamed as old-config.yml");
             $this->saveResource("config.yml");
         } else {
             $this->saveConfig();
         }
-        if (SlapInfo::IS_DEVELOPMENT_BUILD == true){
-            $log->warning(TextFormat::RED."Your SlapBack is in development build! You may expect crash during the plugin. You can make issue about this plugin by visiting plugin github issue!");
-            return;
-        }
     }
 	public function onEnable() :void{
 	    $toggle = $this->getConfig()->get("enabled");
 	    $log = $this->getServer()->getLogger();
-	    if (SlapInfo::PROTOCOL_VERSION == ProtocolInfo::CURRENT_PROTOCOL){
-                $log->info(TextFormat::GREEN."Your SlapBack is Compatible with your version!");
-            } else {
-                $log->info(TextFormat::RED."Your SlapBack isnt Compatible with your version!");
-                $this->getServer()->getPluginManager()->disablePlugin($this);
-                return;
-            }
 	    if (!isset($toggle)){
 	        $log->error("It cant be blank the config!");
 	        $this->getConfig()->set("enabled", true);
 	        return;
 	    } else {
-	     if ($toggle == true){
-	    $log->info("The plugin was enabled!");
+	    if ($toggle == true){
+	        $log->info("The plugin was enabled!");
 		$this->getServer()->getPluginManager()->registerEvents(new SlapListener($this), $this);
 		return;
 	     }
