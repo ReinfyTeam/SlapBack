f642fc00125b18df5aea151619598593b9d199ec
Ace Sobremont <100104970+xqwtxon@users.noreply.github.com>
2022-05-13 19:38:21 +0800

    instanceof Player

--- src/SlapListener.php
+++ src/SlapListener.php
@@ -7,6 +7,7 @@ use slapper\entities\SlapperHuman;
 use slapper\events\SlapperHitEvent;
 use pocketmine\event\Listener;
 use pocketmine\entity\Human;
+use pocketmine\player\Player;
 use pocketmine\entity\animation\ArmSwingAnimation;
 class SlapListener implements Listener {
     
@@ -15,9 +16,6 @@ class SlapListener implements Listener {
     }
     
     public function onSlapperHit(SlapperHitEvent $ev){
-        $toggle = $this->plugin->getConfig()->get("enabled");
-        if ($toggle == false) return;
-        if ($toggle == true){
 		  $entity = $ev->getEntity();
 		  if(!$entity instanceof SlapperHuman){
 			return;
@@ -26,8 +24,10 @@ class SlapListener implements Listener {
 		  if(!$entity instanceof SlapperHuman){
 			return;
 		  }
+                  if(!$entity instanceof Player){
+                         return;
+                  } 
                   $player = $ev->getDamager();
                   $entity->broadcastAnimation(new ArmSwingAnimation($entity), [$player]);  
-          }
-	}
+         } 
 }
